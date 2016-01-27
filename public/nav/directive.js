789b44b5ab451c82c0e3a2f57b8069c31f9fc1d3
joy2king <jiuyizhang91@gmail.com>
2016-01-27 14:12:50 -0500

    ddd

---
+++ public/nav/directive.js
@@ -0,0 +1,13 @@
+(function(){
+
+
+    'use strict';
+
+        angular.module('main').directive('myNav',function(){
+            return {
+                templateUrl:'nav/nav.html',
+                controller:'UserCtrl',
+                controllerAs:'user'
+            };
+        });
+})()
